f1056a6d75a4c758d59c3aac78edff2d2f456d6d
Jebitok <sharonkosgei4@gmail.com>
2021-06-02 17:12:12 +0300

    Add Buttons Component

---
+++ src/components/display.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+function Button({ name }) {
+  return (
+    <button type="button">{name}</button>
+  );
+}
+
+Button.propTypes = { name: PropTypes.string.isRequired };
