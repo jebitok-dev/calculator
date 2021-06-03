4237c49775d454ab131ecfff66d5a2174284688b
Jebitok <sharonkosgei4@gmail.com>
2021-06-03 12:38:55 +0300

    Add Calculatejs Logic

---
+++ src/logic/calculate.js
@@ -0,0 +1,24 @@
+import operate from './operate';
+
+function calculate(calculatorDataObj, buttonName) {
+  let { total, next, operation } = calculatorDataObj;
+  if (buttonName === 'AC') {
+    total = '';
+    next = '';
+    operation = '';
+  } else if (buttonName === '+/-') {
+    total = next * total * -1;
+  } else if (buttonName === '+'
+        || buttonName === '-'
+        || buttonName === '/'
+        || buttonName === '*'
+        || buttonName === '%'
+  ) {
+    operation = !next ? buttonName : null;
+  } else if (buttonName === '=') {
+    operate(total, next, operation);
+  }
+  return { total, next, operation };
+}
+
+export default calculate;
