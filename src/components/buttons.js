15eb64ab5f52b78fb106511e516f161761f3307a
Jebitok <sharonkosgei4@gmail.com>
2021-06-04 13:03:22 +0300

    Add Changes to Buttons Component: Pass ClickHandler

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 
-function Button({ name }) {
+function Button({ name, handleClick }) {
   return (
-    <button type="button">{name}</button>
+    <button type="button" onClick={() => handleClick(name)}>{name}</button>
   );
 }
 
-Button.propTypes = { name: PropTypes.string.isRequired };
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default Button;
