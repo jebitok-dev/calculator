c9da44d7323dde9e5ccaa614ab983f68714d17a0
Jebitok <sharonkosgei4@gmail.com>
2021-06-09 18:39:36 +0300

    Add Quote Component

---
+++ src/components/quote.js
@@ -0,0 +1,13 @@
+const Quote = () => (
+  <div className="centered">
+    <h3>
+      Mathematics is not about numbers, equations, computations, or algorithms: it
+      is about understanding.
+      <br />
+      {' '}
+      -William Paul Thurston
+    </h3>
+  </div>
+);
+
+export default Quote;
