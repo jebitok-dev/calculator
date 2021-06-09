8305fbf2ed316d1c647993cd9c516199b0b7e730
Jebitok <sharonkosgei4@gmail.com>
2021-06-09 18:56:00 +0300

    Add Router Component

---
+++ src/components/router.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './homepage';
+import Calculator from './calculator';
+import Navbar from './navbar';
+import Quote from './quote';
+
+const Router = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/calculator" component={Calculator} />
+      <Route path="/quote" component={Quote} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
