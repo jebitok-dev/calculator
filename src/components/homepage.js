5a7e937f48810cb35cf55eb1eb1bcbf4f4ebd6a9
Jebitok <sharonkosgei4@gmail.com>
2021-06-10 13:04:10 +0300

    Add Changes to Home Layout

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -6,15 +6,44 @@ const Home = () => (
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero vel
       quibusdam veritatis quaerat excepturi exercitationem numquam eligendi ut,
-      mollitia voluptate!
+      mollitia voluptate! Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Corrupti mollitia ullam voluptatem quam exercitationem!
     </p>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti mollitia
-      ullam voluptatem quam exercitationem!
+      ullam voluptatem quam exercitationem! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Corrupti mollitia ullam voluptatem quam
+      exercitationem!
     </p>
     <p>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae nihil
       praesentium reiciendis ut fugiat. Voluptas rem natus aut magni assumenda.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti mollitia
+      ullam voluptatem quam exercitationem!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti mollitia
+      ullam voluptatem quam exercitationem! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Corrupti mollitia ullam voluptatem quam
+      exercitationem!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti mollitia
+      ullam voluptatem quam exercitationem! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Corrupti mollitia ullam voluptatem quam
+      exercitationem!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti mollitia
+      ullam voluptatem quam exercitationem! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Corrupti mollitia ullam voluptatem quam
+      exercitationem!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti mollitia
+      ullam voluptatem quam exercitationem! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Corrupti mollitia ullam voluptatem quam
+      exercitationem!
     </p>
   </div>
 );
