484256807b308eefdeb514a6f6c8d1c81e6a3d75
Jebitok <sharonkosgei4@gmail.com>
2021-06-11 18:57:33 +0300

    Add HomePage Test

---
+++ src/tests/spa/homepage.test.js
@@ -0,0 +1,8 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import Home from '../../components/homepage';
+
+it('should match the Homepage snapshot', () => {
+  const home = renderer.create(<Home />).toJSON();
+  expect(home).toMatchSnapshot();
+});
