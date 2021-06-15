d4189a31a895904b9e589d1e2533db9a0395a75a
Jebitok <sharonkosgei4@gmail.com>
2021-06-15 13:28:58 +0300

    Add Changes to Operate Tests

--- src/components/Error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Error = () => {
-  <div className="centered">
-    <h1>Oops! Page not found!</h1>
-  </div>;
-};
-
-export default Error;
--- src/tests/logic/Operate.test.js
+++ src/tests/logic/Operate.test.js
@@ -6,9 +6,15 @@ test('should add, minus, multiply, divide and give percentage of two numbers', (
   const multiply = operate('8', '5', 'x');
   const divide = operate('10', '5', '/');
   const percent = operate('100', '40', '%');
+  const zero = operate('100', '0', '/');
+  const percentage = operate('25', '%');
+  const decimal = operate('3.3.3.3.3');
   expect(add).toBe('13');
   expect(minus).toBe('3');
   expect(multiply).toBe('40');
   expect(divide).toBe('2');
   expect(percent).toBe('1');
+  expect(zero).toBe('Infinity');
+  expect(percentage).toBe('0');
+  expect(decimal).toBe('0');
 });
